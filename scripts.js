6bd88d002536b591b20e7f97a81db51694f807aa
Gavin <gmferise@gmail.com>
2019-11-11 11:09:42 -0500

    added sheets scope

--- scripts.js
+++ scripts.js
@@ -1,18 +1,19 @@
 // Google Auth Config
 var GoogleAuth;
-var SCOPE = "https://www.googleapis.com/auth/drive.metadata.readonly";
+;
 function handleClientLoad() {
 	gapi.load("client:auth2", initClient);
 }
 
 function initClient() {
 	// do not place docs directly in the array because javascript will stroke out
-	var docs = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+	var docs = ["https://www.googleapis.com/discovery/v1/apis/sheets/v4/rest";
+	var scopes = ["https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/spreadsheets"]
 	gapi.client.init({
 		"apiKey":"AIzaSyDIptkXtN8vcrOr5LPBvk21WuAk8UmVwAs",
-		"discoveryDocs":[docs],
+		"discoveryDocs":docs,
 		"clientId":"1031491199015-pbjmtfn9kj0tvcl24k7vntelua6glb90.apps.googleusercontent.com",
-		"scope":SCOPE
+		"scope":scopes
 	}).then(function() {
 		GoogleAuth = gapi.auth2.getAuthInstance();
 		GoogleAuth.isSignedIn.listen(updateSigninStatus);
@@ -21,7 +22,7 @@ function initClient() {
 		setSigninStatus();
 		
 		$("#sign-in-or-out-button").click(function() {
-			handleAuthClick();
+			toggleAuth();
 		});
 		
 		$("#revoke-access-button").click(function() {
@@ -30,7 +31,7 @@ function initClient() {
 	});
 }
 
-function handleAuthClick() {
+function toggleAuth() {
 	if (GoogleAuth.isSignedIn.get()) {
 		GoogleAuth.signOut();
 	}
@@ -46,46 +47,17 @@ function revokeAccess() {
 function setSigninStatus(isSignedIn) {
 	var user = GoogleAuth.currentUser.get();
 	if (GoogleAuth.isSignedIn.get()) {
-		$('#sign-in-or-out-button').html('Sign out');
+		$('#sign-in-or-out-button').html('Sign Out');
 		$('#revoke-access-button').css('display', 'inline-block');
-		$('#auth-status').html('You are currently signed in and have granted ' +
-			'access to this app.');
+		$('#auth-status').html('(Currently Signed In.)');
 	}
 	else {
-		$('#sign-in-or-out-button').html('Sign In/Authorize');
+		$('#sign-in-or-out-button').html('Sign In');
 		$('#revoke-access-button').css('display', 'none');
-		$('#auth-status').html('You have not authorized this app or you are ' +
-			'signed out.');
+		$('#auth-status').html('(Curently Signed Out.)');
 	}
 }
 
 function updateSigninStatus(isSignedIn) {
 	setSigninStatus();
 }
-
-// Google Auth Sign-In
-function onSignIn(googleUser) {
-	// Useful data for your client-side scripts:
-	var profile = googleUser.getBasicProfile();
-	console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-	console.log('Full Name: ' + profile.getName());
-	console.log('Given Name: ' + profile.getGivenName());
-	console.log('Family Name: ' + profile.getFamilyName());
-	console.log("Image URL: " + profile.getImageUrl());
-	console.log("Email: " + profile.getEmail());
-
-	// The ID token you need to pass to your backend:
-	var id_token = googleUser.getAuthResponse().id_token;
-	console.log("ID Token: " + id_token);
-	document.getElementById("sign-out-button").classList.remove("hidden");
-}
-
-// Google Auth Sign-Out
-function signOut() {
-	var auth2 = gapi.auth2.getAuthInstance();
-	auth2.signOut().then(function () {
-		console.log('User signed out.');
-		document.getElementById("sign-out-button").classList.add("hidden");
-	});
-}
-
