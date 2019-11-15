24180522e958f0035e3c34ef657a4312c933c630
Gavin <gmferise@gmail.com>
2019-11-15 11:20:34 -0500

    appdata test

--- scripts.js
+++ scripts.js
@@ -1,21 +1,24 @@
 // Google Auth Config
 var GoogleAuth;
-;
-function handleClientLoad() {
+
+
+function handleClientLoad() { // Called from HTML when API loads
 	gapi.load("client:auth2", initClient);
 }
 
-function initClient() {
+function initClient() { // Generates auth client instance, stored in GoogleAuth
 	// do not place docs directly in the array because javascript will stroke out
 	var docs = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest","https://www.googleapis.com/discovery/v1/apis/sheets/v4/rest"];
-	gapi.client.init({
+	gapi.client.init({ // Initialize a client with these properties
 		"apiKey":"AIzaSyDIptkXtN8vcrOr5LPBvk21WuAk8UmVwAs",
 		"discoveryDocs":docs,
 		"clientId":"1031491199015-pbjmtfn9kj0tvcl24k7vntelua6glb90.apps.googleusercontent.com",
-		"scope":"https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets"
+		"scope":"https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/drive.appdata"
 	}).then(function() {
 		GoogleAuth = gapi.auth2.getAuthInstance();
 		GoogleAuth.isSignedIn.listen(updateSigninStatus);
+		
+		loadAppData();
 		
 		var user = GoogleAuth.currentUser.get();
 		setSigninStatus();
@@ -23,11 +26,45 @@ function initClient() {
 		$("#sign-in-or-out-button").click(function() {
 			toggleAuth();
 		});
-		
-		$("#revoke-access-button").click(function() {
-			revokeAccess();
-		});
 	});
+}
+
+function loadAppData() {
+	// Search for our appdata file
+	var exists = false;
+	drive.files.list({
+		spaces: 'appDataFolder',
+		fields: 'nextPageToken, files(id, name)',
+		pageSize: 100
+	}, function (err, res) {
+		if (err) { console.error(err); }
+		else {
+			res.files.forEach(function (file) {
+				console.log('Found file:', file.name, file.id);
+				if (file.name == 'config.json') { exists = true; }
+			});
+		}
+	});
+	
+	// Not found, create it
+	if (!exists) {
+		var fileMetadata = {
+			'name': 'config.json',
+			'parents': ['appDataFolder']
+		};
+		var media = {
+			mimeType: 'application/json',
+			body: fs.createReadSstream('files/config.json')
+		};
+		drive.files.create({
+			resource: fileMetadata,
+			media: media,
+			fields: 'id'
+		}, function (err, file) {
+			if (err) { console.error(err); }
+			else { console.log('File Id:', file.id); }
+		});
+	}
 }
 
 function toggleAuth() {
@@ -39,20 +76,14 @@ function toggleAuth() {
 	}
 }
 
-function revokeAccess() {
-	GoogleAuth.disconnect();
-}
-
-function setSigninStatus(isSignedIn) {
+function setSigninStatus(isSignedIn) { // Sets status message
 	var user = GoogleAuth.currentUser.get();
 	if (GoogleAuth.isSignedIn.get()) {
 		$('#sign-in-or-out-button').html('Sign Out');
-		$('#revoke-access-button').css('display', 'inline-block');
 		$('#auth-status').html('(Currently Signed In.)');
 	}
 	else {
 		$('#sign-in-or-out-button').html('Sign In');
-		$('#revoke-access-button').css('display', 'none');
 		$('#auth-status').html('(Curently Signed Out.)');
 	}
 }
@@ -60,3 +91,23 @@ function setSigninStatus(isSignedIn) {
 function updateSigninStatus(isSignedIn) {
 	setSigninStatus();
 }
+
+// Sheets Calls
+function testCall() {
+      var params = {
+        spreadsheetId: 'os-test',
+        range: 'A1:C3',
+        
+        // The default render option is ValueRenderOption.FORMATTED_VALUE.
+        valueRenderOption: '',
+        // The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
+        dateTimeRenderOption: '',
+      };
+
+      var request = gapi.client.sheets.spreadsheets.values.get(params);
+      request.then(function(response) {
+        console.log(response.result);
+      }, function(reason) {
+        console.error('error: ' + reason.result.error.message);
+      });
+    }
