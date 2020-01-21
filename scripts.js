120315edcc98b2bbaf6d273e85fcdeccb975ca61
Gavin Ferise <46463513+gmferise@users.noreply.github.com>
2020-01-21 10:43:35 -0500

    no params

--- scripts.js
+++ scripts.js
@@ -154,10 +154,10 @@ function importDatabase(url){
 }
 
 function getDatabases(){
-	// Do not place queryParams directly in the array, must be evaluated beforehand
-	var queryParams = "mimeType='application/vnd.google-apps.spreadsheet' and '"+GoogleAuth.currentUser.get().getBasicProfile().getEmail()+"' in writers and name contains '[OsDB]'";
+	// Do not place params directly in the array, must be evaluated beforehand
+	// var params = "mimeType='application/vnd.google-apps.spreadsheet' and '"+GoogleAuth.currentUser.get().getBasicProfile().getEmail()+"' in writers and name contains '[OsDB]'";
 	gapi.client.drive.files.list({
-		q: queryParams,
+		// q: params,
 	}).then(function(response) {
 		console.log("Response", response.result.files);
     },function(err) { console.error("Failed to search Drive for Databases"); });
