f2f0d7513ff056891491af70485d401b0613d47a
Gavin Ferise <46463513+gmferise@users.noreply.github.com>
2020-02-27 20:43:51 -0500

    fixed flicker

--- scripts.js
+++ scripts.js
@@ -50,8 +50,7 @@ function initClient() {
 		"scope":"https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets"
 	}).then(function() {
 		GoogleAuth = gapi.auth2.getAuthInstance();
-		GoogleAuth.signOut();
-		onAuthUpdate();
+		GoogleAuth.signOut().then(onAuthUpdate());
 		GoogleAuth.isSignedIn.listen(onAuthUpdate);
 	});
 }
