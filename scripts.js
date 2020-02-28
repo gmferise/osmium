d6ca2e860aa6c80122aad80fe298c4995a3519cf
Gavin Ferise <46463513+gmferise@users.noreply.github.com>
2020-02-27 20:32:23 -0500

    choose account every time test

--- scripts.js
+++ scripts.js
@@ -50,6 +50,7 @@ function initClient() {
 		"scope":"https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets"
 	}).then(function() {
 		GoogleAuth = gapi.auth2.getAuthInstance();
+		GoogleAuth.signOut();
 		GoogleAuth.isSignedIn.listen(onAuthUpdate);
 		onAuthUpdate();
 	});
